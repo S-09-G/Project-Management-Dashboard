92a3d05066fceac7f6b3fcf2ff5196e960dd7c8c
Shivansh <guptashivansh2004@gmail.com>
2024-06-02 13:21:02 +0530

    changes to the UI of Dashboard added some animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './ThemeContext';
 import Login from './Login';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Signup from './Signup';
 import Home from './Home';
-import Settings from './Settings'
+import Settings from './Settings';
 import Analysis from './Analysis';
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/settings' element={<Settings />}></Route>
-        <Route path='/analysis' element={<Analysis />}></Route>
-      </Routes>
-    </BrowserRouter> 
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/analysis' element={<Analysis />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
